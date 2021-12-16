7826991eef885aab291d6ddf7797f8a79840ada8
Purewhite2019 <729170579@qq.com>
2021-12-16 14:41:11 +0800

    Dataset of e2e: bug fixed; benchmark.py: minor bug fixed; xudong-e2e: added and revised

--- scripts/train_mxfold2.sh
+++ scripts/train_mxfold2.sh
@@ -1,6 +1,9 @@
 SEED=0
-TYPE="MixC"
-TRANSFORMER=0 # 0 for lstm, 1 for transformer
+TYPE="Turner"
+TRANSFORMER=1 # 0 for lstm, 1 for transformer
+GROUP='mxfold2_turner_transformer_hinge_mix'
+LOSS='hinge_mix' # or 'hinge_mix'
+EXP=$GROUP
 
 # model type for mxfold2 can be:
 # Turner, Zuker, ZukerC, ZukerL, ZukerS, 
@@ -16,7 +19,10 @@ python train.py --model mxfold2 \
                 --batch-size 1 \
                 --num-transformer-layers $TRANSFORMER \
                 --num-transformer-att 4 \
+                --loss-func $LOSS \
+                --exp $EXP \
+                --epoch 20 
                 --wandb \
                 --wandb_project RNA_AI \
                 --wandb_name $SEED \
-                --wandb_group mxfold2_mixc_lstm
+                --wandb_group $GROUP
